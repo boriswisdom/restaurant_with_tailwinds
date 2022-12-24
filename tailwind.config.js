fe72a484b00abfb69042c8ab58378983dfee014c
Boris Wisdom Azambou <borisnandong9@gmail.com>
2022-12-24 12:44:14 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#06b6d4',
+        secondary: {
+        100: '#9333ea',
+        200: '#2563eb'
+      }
+      },
+      fontSize:{
+        pro: '10rem'
+      },
+      fontFamily:{
+        primary: 'Nunito'
+      }
+    },
+  },
+  plugins: [],
+}
